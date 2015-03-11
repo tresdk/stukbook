e0c0aa6e0b6756b688230bbc01acbde870453685
tresdk <tresdk@tresdk.com>
2015-03-11 16:37:36 +0100

    Added seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(username: "Pino", email: "pinofanclub@hotmail.com", password: "12345678", password_confirmation: "12345678")
+User.create(username: "Pino2", email: "pinofanclub@hotmail.com", password: "12345678", password_confirmation: "12345678")
+User.create(username: "Pino3", email: "pinofclb@hotmail.com", password: "12345678", password_confirmation: "12345678")
+User.create(username: "Pino5", email: "pinanclub@hotmail.com", password: "12345678", password_confirmation: "12345678")
+User.create(username: "Tiago91", email: "tiagom91@gmail.com", password: "12345678", password_confirmation: "12345678")
+p "Test users created"
