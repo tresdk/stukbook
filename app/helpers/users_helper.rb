f1f8b60e43dc9e342718893eb09bf21cf9ed2f6c
tresdk <tresdk@tresdk.com>
2015-03-11 17:31:41 +0100

    Added friendship status detection

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+
+  def action_buttons(user)
+    case current_user.friendship_status(user) when "friends"
+      "Remove Friendship"
+    when "pendind"
+      "Cancel request"
+    when "requested"
+      "Accept or deny"
+    when "not_friends"
+      "Add as a friend"
+    end      
+  end
+
 end
