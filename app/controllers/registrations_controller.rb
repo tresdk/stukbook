20dae569f55cf08ee86079e50907fc9d1f8eeed0
tresdk <tresdk@tresdk.com>
2015-03-11 11:11:57 +0100

    added extra user attribs

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,7 @@ private
 		params.require(:user).permit(:email, :password, :password_confirmation, :username)	
 	end
 	def account_update_params
-		params.require(:user).permit(:email, :password, :password_confirmation, :username, :current_password)	
+		params.require(:user).permit(:email, :password, :password_confirmation, :username, :current_password, :bio, :gender, :age)	
 	end
 	
 end
