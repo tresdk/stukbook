04ffb9aacd31b990b26e48f57fd9145e6dbb6585
tresdk <tresdk@tresdk.com>
2015-03-12 12:47:22 +0100

    created post model. Added html_ready gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :username
   has_many :friendships, dependent: :destroy
   has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id", dependent: :destroy
+  has_many :posts, dependent: :destroy
 
   def request_friendship(user_2)
     self.friendships.create(friend: user_2)
