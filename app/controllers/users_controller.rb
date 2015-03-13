fecc98186e89fa08973637452a5a90a4707f95b0
tresdk <tresdk@tresdk.com>
2015-03-13 11:07:50 +0100

    final touches

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
   before_action :set_user, only: [:show]
+  before_action :get_counts, only: [:index]
   def index
   	case params[:people] when "friends"
       @users = current_user.active_friends
@@ -19,7 +20,10 @@ class UsersController < ApplicationController
   end
   
   private
-
+  def get_counts
+   @friend_count = current_user.active_friends.size
+   @pending_count = current_user.pending_friend_requests_to.map(&:friend).size
+  end
   def set_user
     @user = User.find_by(username: params[:id])    
   end
